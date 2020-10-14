846044036604a1405e93254eac090ada138a071e
Donne Dylan <dylan_donne@ens.univ-artois.fr>
2020-10-14 15:51:03 +0200

    ✨ Add custom logger

--- src/program.c
+++ src/program.c
@@ -16,6 +16,29 @@ Player player = {(Vector2){200, 200}, (Vector2){100.f, 100.f}, 1.f, 1.f, (Vector
 Camera2D camera = { 0 };
 char coords[10];
 
+// Custom logger
+static void logger(int msgType, const char *text, va_list args)
+{
+    char timeStr[64] = { 0 };
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
+
+    strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", tm_info);
+    printf("[%s] ", timeStr);
+
+    switch (msgType)
+    {
+        case LOG_INFO: printf("[INFO] : "); break;
+        case LOG_ERROR: printf("[ERROR]: "); break;
+        case LOG_WARNING: printf("[WARN] : "); break;
+        case LOG_DEBUG: printf("[DEBUG]: "); break;
+        default: break;
+    }
+
+    vprintf(text, args);
+    printf("\n");
+}
+
 //------------------------------------------------------------------------------------
 // Module Functions Declaration (local)
 //------------------------------------------------------------------------------------
@@ -36,6 +59,7 @@ int main(void)
     //---------------------------------------------------------
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    SetTraceLogCallback(logger);
     InitWindow(screenWidth, screenHeight, "50NuancesDeCodes - by IMT GREY TEAM");
 
     InitGame();
